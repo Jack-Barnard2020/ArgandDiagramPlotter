60bd2fb6d8918f60844d0482ff37c6ba0280d03b
Jack_B <jackpeterbarnard@icloud.com>
2023-09-24 21:32:45 +0100

    Commented on ComplexNumber.py function

--- ComplexNumber.py
+++ ComplexNumber.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 def ComplexNumber():
     x = float(input("Input Real Part "))
     y = float(input("Input Complex Part "))
+
     plt.xlim(-((np.absolute(x))+5), np.absolute(x)+5)
     plt.ylim(-((np.absolute(y))+5), np.absolute(y)+5)
+    #limits x and y view to +/- from point
+
     plt.plot(x, y, marker="o", markersize=20)
+    #plots point on grid
