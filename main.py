13150260b67345eb1b57f10023740ff7a4683cd9
Jack_B <jackpeterbarnard@icloud.com>
2023-09-26 21:55:30 +0100

    File SImplfication

--- main.py
+++ main.py
@@ -6,4 +6,26 @@ Menu = [1,2,3]
 MenuOption = int()
 AnotherPlot = str()
 
-plot.Plot()
+def PlotComplexNumber():
+    Real = float(input("Input the real part of a complex number? "))
+    Complex = float(input("Input the complex part of a complex number? "))
+    plot.ComplexNumber(Real, Complex)
+
+def PlotArgument():
+    ArgAngle = int(input("Input numerator of fractional value of pi? "))/int(input("Input denominator of fractional value of pi? "))
+    #asks for a fractional value of pi with out p, pi is multiplied onto value in ArgumentOFACOMplex
+    
+    ArgX = float(input("Input the real part of a complex number? "))
+    ArgY = float(input("Input the complex part of a complex number? "))
+    plot.ComplexArg(ArgAngle, ArgX, ArgY)
+
+def PlotAbslouteValue():
+    CenterX = -float(input("real part? "))
+    CenterY = -float(input("complex part? "))
+    #opposite sign of the part will be the center
+
+    Radius = float(input("absloute value? "))
+    #radius is equal to the absloute value of a complex number, when plotted on an argand diagram
+
+    plot.ComplexAbsloute(CenterX, CenterY, Radius)
+
