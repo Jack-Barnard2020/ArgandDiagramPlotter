f50007d8f6244c8229305c9dfb6ec78029b28c12
Jack_B <jackpeterbarnard@icloud.com>
2023-09-30 15:02:59 +0100

    Menu Repeat and error handling

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def PlotArgument():
     
     ArgX = float(input("Input the real part of a complex number? "))
     ArgY = float(input("Input the complex part of a complex number? "))
+    #sets point at which line will go from
     plot.ComplexArg(ArgAngle, ArgX, ArgY)
 
 def PlotAbslouteValue():
@@ -33,9 +34,12 @@ def PlotAbslouteValue():
 
 def menu():
     MenuOption = input("What would you like to do? \n   1. Draw an absloute value \n   2. Draw an argument \n   3. Plot a complex number ")
+    #asks for user input 
     try:
         MenuOption = int(MenuOption)
+        #tries to make input an integer such that it can be prosseed by IFTT
         if MenuOption in AvailbleMenuOptions:
+                #checks that input is in range
                 if MenuOption == 1:
                     PlotAbslouteValue()
                 elif MenuOption == 2:
@@ -48,29 +52,45 @@ def menu():
         else:
             print("ERROR \nnumber not in range, please try again")
             menu()
+            #user doesn\t input 1,2,3 but is still a number
     except TypeError:
         print("ERROR \nYou have not inputted a number, please try again")
         menu()
     except ValueError:
         print("ERROR \nYou have not inputted a number, please try again")
         menu()
+    #except errors print when an invalid input is, ie not a number
+
+def anotherplot():
+    AnotherPlot = input("Would you like to plot another propery before we draw the graph? y/n? ")
+    #user input can break the while loop casuing the Argand Diagram to be plotted
+    try:
+        AnotherPlot = AnotherPlot.lower()
+        #checks for a valid input then breaks or runs program acordingly
+        if AnotherPlot in AvalibleRunningOptions:
+            if AnotherPlot == "n":
+                running = False
+            elif AnotherPlot == "y":
+                running = True
+            else:
+                print("ERROR \nSomething else went wrong, please try again")
+                anotherplot()
+        else:
+                print("ERROR \nNot a valid input, please try agin")
+                anotherplot()
+    except ValueError:
+        print("ERROR \nNot a letter, please try again")
+        #fail prints when a number or symbol is inputed
+        anotherplot()
+    return running
 
 def run():
     running = True
     while running == True:
+        #infinite while loop
         menu()
-        AnotherPlot = input("Would you like to plot another propery before we draw the graph? y/n? ")
-        try:
-            AnotherPlot = AnotherPlot.lower()
-            if AnotherPlot in AvalibleRunningOptions:
-                if AnotherPlot == "y":
-                    running = True
-                else:
-                    running = False
-            else:
-                print("ERROR \n not a valid input, please try agin")
-        except ValueError:
-            print("ERROR \n Not a letter, please try again")
+        running = anotherplot()
+
 
 
 
